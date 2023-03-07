0c131ded33af85d2362bb5c0dc7f79f6f147638e
juancrls <ju4ncrls@gmail.com>
2023-03-07 00:44:08 -0300

    Minor changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCWhe5TonIr1pEHipybmrxcOppj0Wkb0Lc",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
